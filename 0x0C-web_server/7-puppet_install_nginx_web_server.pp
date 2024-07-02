c2374ea0be93e4dc48a89b7dd459cbfe18a5c6d3
jan-ice17 <judysjewel17@gmail.com>
2024-07-02 16:38:47 -0700

    executable fiels added

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,12 @@
+# A manuscript that installs nginx using puppet
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
