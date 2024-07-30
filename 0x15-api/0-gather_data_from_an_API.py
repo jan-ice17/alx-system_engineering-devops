1636e511519aae2e8e818c039248b95210b7f14e
jan-ice17 <judysjewel17@gmail.com>
2024-07-30 12:46:00 +0300

    Indentation fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -37,8 +37,8 @@ def main():
             TASK_TITLE.append(j.get("title"))
 
     # Print the todo list progress
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks (\
-            {NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+            f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
     for title in TASK_TITLE:
         print('\t ' + title)
 
