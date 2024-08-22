c835d933738ba1ac3c4270ad4a6b808e51f5a8d7
jan-ice17 <judysjewel17@gmail.com>
2024-08-23 00:57:50 +0300

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Adjust the hard file limit for the holberton user to prevent login and file access errors
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Adjust the soft file limit for the holberton user to ensure smooth operation
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
